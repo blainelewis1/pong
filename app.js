20c9c235cf7a5b66c19199aac013d8fb12a970c7
Blaine Lewis <blaine.lewis.1@outlook.com>
2019-04-25 11:12:40 -0600

    formatted files

--- app.js
+++ app.js
@@ -10,21 +10,27 @@ var httpServer = http.createServer(app).listen(3000);
 var waiting;
 var games = [];
 
-var wsServer = new WebSocketServer({"httpServer" : httpServer});
-wsServer.on("request", function(request) {
+var wsServer = new WebSocketServer({
+    "httpServer": httpServer
+});
+
+wsServer.on("request", function (request) {
     var connection = request.accept(null, request.origin);
-        
-    connection.on("close", function() {
-        if(waiting === connection) {
+
+    connection.on("close", function () {
+        if (waiting === connection) {
             waiting = undefined;
         }
-    }); 
+    });
 
-    if(waiting) {
+    if (waiting) {
         games.push(new Game(waiting, connection));
         waiting = undefined;
     } else {
         waiting = connection;
-        waiting.sendUTF(JSON.stringify({"type": "message", "text" : "Waiting for an opponent..."}));
+        waiting.sendUTF(JSON.stringify({
+            "type": "message",
+            "text": "Waiting for an opponent..."
+        }));
     }
 });
